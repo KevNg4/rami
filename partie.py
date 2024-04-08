1959b3b44e2429f04371d8342d1eff56d62e3431
Kevin Nguyen <kevin.nguyen2504@icloud.com>
2024-04-08 17:08:58 +0200

    Input nbre joueurs, liste d'objets Joueur

--- partie.py
+++ partie.py
@@ -1,8 +1,21 @@
 from paquet import Paquet
+from joueur import Joueur
 
 print('Bienvenue dans le Rami!\n')
 
-nb_joueurs = int(input('Combien de joueurs ? : '))
+while True:
+    try:
+        nb_joueurs = int(input("Combien de joueurs ? : "))
+        if 2 <= nb_joueurs <= 6:
+            break
+        elif nb_joueurs > 6:
+            print("Le rami se joue à 6 maximum")
+        else:
+            print("Erreur : Vous devez entrer un nombre de joueurs")
+    except ValueError:
+        print("Erreur : Vous devez entrer un nombre de joueurs")
+
+joueurs = [Joueur() for i in range(nb_joueurs)]
 
 paquet = Paquet()
 paquet.melanger()
